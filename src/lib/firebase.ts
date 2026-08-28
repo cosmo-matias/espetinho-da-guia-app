fc6505a57627341639e93aadbaebf25fade89c0d
Cosmo Matias Gomes <ducosmo@MacBook-Pro-de-Cosmo.local>
2026-08-28 17:15:41 -0300

    chore: setup inicial do projeto com nextjs, tailwind e firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,4 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+// Adicionaremos as credenciais na próxima fase.
